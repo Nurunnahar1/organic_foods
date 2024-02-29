4217d73f14a56b378ed22bbbee9a6c1031cf2973
Nurunnahar <nurunnahar9081@gmail.com>
2024-02-29 23:51:37 +0600

    script and style file setup done

---
+++ resources/views/backend/components/style.blade.php
@@ -0,0 +1,24 @@
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+  <!-- End fonts -->
+
+	<!-- core:css -->
+	<link rel="stylesheet" href="{{ asset('assets/backend/vendors/core/core.css') }}">
+	<!-- endinject -->
+
+	<!-- Plugin css for this page -->
+	<link rel="stylesheet" href="{{ asset('assets/backend/vendors/flatpickr/flatpickr.min.css') }}">
+	<!-- End plugin css for this page -->
+
+	<!-- inject:css -->
+	<link rel="stylesheet" href="{{ asset('assets/backend/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('assets/backend/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<!-- endinject -->
+
+  <!-- Layout styles -->  
+	<link rel="stylesheet" href="{{ asset('assets/backend/css/demo2/style.css') }}">
+  <!-- End layout styles -->
+
+  <link rel="shortcut icon" href="{{ asset('assets/backend/images/favicon.png') }}" />
