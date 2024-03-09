63904782008f3018827c240b155ba1b4a0093d27
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-09 21:51:03 +0600

    testimonial frontend method

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,9 +10,7 @@ class HomeController extends Controller
 {
 
     public function home(){
-        $testimonials = Testimonial::where('is_active',1)->latest()->limit(4)->select(['id','client_name','client_designation','client_message','client_image'])->get();
-
-        // return $testimonials;
+        $testimonials = Testimonial::where('is_active',1)->latest()->limit(4)->select(['id','client_name','client_designation','client_message','client_image'])->get(); 
         return view('frontend.pages.home',compact('testimonials'));
     }
 
