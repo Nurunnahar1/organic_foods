beb50675110c6d38be9f1a791657bea580573e16
Nurunnahar <nurunnahar9081@gmail.com>
2024-04-21 22:25:30 +0600

    breadcrumb html

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
        $categories = Category::where('is_active', 1)->latest()->limit(12)->select(['id', 'title', 'category_image',
        'slug'])->get();
        $products = Product::where('is_active', 1)->latest()->limit(8)->select(['id', 'name', 'short_description',
-       'product_price', 'product_image'])->get();
+       'product_price', 'product_image','product_rating'])->get();
 
         return view('frontend.pages.home',compact('testimonials','categories','products'));
     }
